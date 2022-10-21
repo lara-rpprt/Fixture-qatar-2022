ea2a84ae155be64a9b4c98caa7e034e50180c047
julabrego <abrego.jul@gmail.com>
2022-10-21 13:54:44 -0300

    Se borra ventana de ejemplo

--- src/fixture/Fixture.java
+++ src/fixture/Fixture.java
@@ -1,16 +1,8 @@
 package fixture;
 
-import fixture.model.Equipo;
-import fixture.model.Estadio;
-import fixture.model.Fase;
-import fixture.model.Grupo;
 import fixture.model.Partido;
 import fixture.repository.ObjectIO;
-import fixture.repository.PartidosMigrations;
-import fixture.vista.Ventana;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Fixture {
 
@@ -19,12 +11,10 @@ public class Fixture {
         ArrayList<Partido> partidos;
         partidos = (ArrayList<Partido>) objectIO.ReadObjectFromFile("partidos");
 
-        // Ventana ventana = new Ventana();
-        //ventana.setVisible(true);
-
         for (Partido partido : partidos) {
             System.out.println(partido);
         }
+       
 
     }
 
